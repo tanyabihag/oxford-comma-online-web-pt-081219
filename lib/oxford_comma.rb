57890b01f9353ee2f66c9728b437fdec51de0ec8
Tanya Bihag <tbihag@gmail.com>
2019-08-21 20:39:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
-  array = ["Kiwi", "durian", "starfruit", "mangos", "dragon fruits"].join(" ")
+   ["Kiwi", "durian", "starfruit", "mangos", "dragon fruits"].join(" ")
 end
 
